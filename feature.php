bce857ad18dabb6831beb4b235ce83b580802882
Alex Nucci <anucci@suffolk.edu>
2018-03-21 14:12:29 -0400

    added title

--- feature.php
+++ feature.php
@@ -4,9 +4,13 @@
 yet another line of comment*/
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo "<h1>This is our feature page!</h1>"
 =======
 >>>>>>> added comment
+=======
+echo "<h1>This is our feature page!</h1>"
+>>>>>>> added title
 phpinfo();
 
 

8d3d5a9a23e71dcd6671b9a28a20f56ef195a0d0
Alex Nucci <anucci@suffolk.edu>
2018-03-21 14:11:31 -0400

    added comment

--- feature.php
+++ feature.php
@@ -3,7 +3,10 @@
 /* This is a multi line comment in PHP with 
 yet another line of comment*/
 
+<<<<<<< HEAD
 echo "<h1>This is our feature page!</h1>"
+=======
+>>>>>>> added comment
 phpinfo();
 
 
